d8262b1cc93383c4c735cc1eac2cc92a9f5e4502
Andrey <andrey.liubashchenko@gmail.com>
2023-04-17 00:59:48 +0300

    first commit

---
+++ src/pages/Details.jsx
@@ -0,0 +1,36 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
+import { loadCountryByName, clearDetails } from '../store/details/details-actions';
+import { selectCurrentCountry, selectDetails } from '../store/details/details-selector';
+
+
+export const Details = () => {
+  const { name } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {currentCountry, error, status} = useSelector(selectDetails)
+
+  useEffect(() => {
+    dispatch(loadCountryByName(name));
+
+    return () => {
+      dispatch(clearDetails());
+    }
+  }, [name, dispatch]);
+
+  return (
+    <div>
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack /> Back
+      </Button>
+      {status === 'loading' && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
+      {currentCountry && <Info push={navigate} {...currentCountry} />}
+    </div>
+  );
+};
